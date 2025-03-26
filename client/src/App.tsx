07cbaf07d306aeb938f54ecca3844cf228cb0be5
Kishan pithiya <pithiyakishan9437@gmail.com>
2025-03-26 23:12:06 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className='bg-orange hover:bg-hoverOrange'>lets build food app</Button>
+    </>
+  )
+}
+
+export default App
